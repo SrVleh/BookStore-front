303b16900ab44f860ff19538e5674e25e9c89c89
TrueVale <srvleh@gmail.com>
2023-04-13 12:10:03 +0200

    [DEV-0013] Improved layout and input types

---
+++ src/components/books/Book.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-container">
+      <div class="book" style="background-image: url('https://upload.wikimedia.org/wikipedia/en/5/56/TheNameoftheWind_cover.jpg')">
+          <img src="../../../public/book-logo.svg" alt="" style ="color: white">
+          <div class="book-info">
+              <h2 class="title">Title</h2>
+              <p class="author">Author</p>
+              <div class="actions">
+                  <p class="price">23€</p>
+                  <button class="buy-btn">Buy</button>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Book"
+}
+</script>
+
+<style scoped>
+    .book {
+        width: 23rem;
+        height: 35rem;
+    }
+</style>
--- src/components/books/EditBook.vue
+++ src/components/books/EditBook.vue
@@ -4,8 +4,9 @@
             <h1>Edit book</h1>
             <input class="custom-input" type="text" placeholder="Title">
             <input class="custom-input" type="text" placeholder="Author">
-            <input class="custom-input" type="text" placeholder="Price">
+            <input class="custom-input" type="number" placeholder="0">
             <input class="custom-input" type="url" placeholder="Image url">
+            <textarea class="custom-area" type="text" placeholder="Synopsis"/>
             <div class="actions-container">
                 <button class="custom-btn">Confirm</button>
                 <button class="custom-btn cancel">Cancel</button>
