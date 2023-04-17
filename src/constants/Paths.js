d98cfc756e000409a13b120ce8a39ae088779bc1
TrueVale <srvleh@gmail.com>
2023-04-17 17:08:09 +0200

    [DEV-0022] Update book

--- src/constants/Paths.js
+++ src/constants/Paths.js
@@ -3,7 +3,7 @@ const Paths = {
     BOOKS_LIST: '/books-listing',
     NEW_BOOK: '/new-book',
     SHOW_BOOK: '/book/:id',
-    UPDATE_BOOK: '/update-book/:id',
+    EDIT_BOOK: '/edit-book/:id',
 }
 
 export default Paths
---
+++ src/constants/RawPaths.js
@@ -0,0 +1,9 @@
+const RawPaths = {
+    HOME: '/',
+    BOOKS_LIST: '/books-listing',
+    NEW_BOOK: '/new-book',
+    SHOW_BOOK: '/book/',
+    EDIT_BOOK: '/edit-book/',
+}
+
+export default RawPaths
