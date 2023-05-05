1a675a49531f0afa404a1a829f346c5e3119d8f0
TrueVale <srvleh@gmail.com>
2023-05-05 17:45:21 +0200

    [DEV-0038] Update profile pic

---
+++ src/components/user/EditProfilePic.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="page-container">
+      <div class="edit-prof-container">
+          <div v-if="prof_pic" class="profile-pic-selected" :style="{ backgroundImage: `url(${ prof_pic })`}"></div>
+          <input v-model="prof_pic" class="custom-input" type="text" placeholder="Profile pic URL">
+          <button class="custom-btn" @click="updateProfilePicture">Update!</button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+  import UserDataController from "../../controllers/UserDataController.js";
+
+  const prof_pic = ref('')
+  const updateProfilePicture = () => {
+      UserDataController.UpdateProfilePic(prof_pic.value)
+  }
+</script>
+
+<style scoped>
+
+.page-container {
+    flex-direction: column;
+    align-items: center;
+
+    .edit-prof-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        min-width: 35%;
+
+        .custom-input {
+            width: 100%;
+        }
+
+        .custom-btn {
+            margin: 0;
+        }
+
+        .profile-pic-selected {
+            width: 10rem;
+            height: 10rem;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    }
+}
+
+</style>
--- src/components/user/ProfilePage.vue
+++ src/components/user/ProfilePage.vue
@@ -2,6 +2,7 @@
   <div class="page-container">
       <div class="profile-data-section">
           <div class="user-data" v-if="store.state.userData">
+            <router-link :to="'/edit_profile_pic/'" class="edit-pic"></router-link>
             <div v-if="store.state.userData.profile_pic != null" class="user-profile-picture" :style="{ backgroundImage: `url(${store.state.userData.profile_pic})` }"></div>
             <div v-else class="user-profile-picture" :style="{ backgroundImage: `url(https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg)` }"></div>
             <p class="username"> {{ store.state.userData.username }} </p>
@@ -12,7 +13,7 @@
 </template>
 
 <script setup>
-  import { store } from "../state/index.js";
+  import { store } from "../../state/index.js";
 </script>
 
 <style scoped>
@@ -28,11 +29,23 @@
           gap: 2rem;
 
           .user-data {
+              position: relative;
               display: flex;
               flex-direction: column;
               align-items: center;
               width: 25%;
               height: 100%;
+
+              .edit-pic {
+                  position: absolute;
+                  width: 1.6rem;
+                  height: 1.6rem;
+                  right: 0;
+                  background-image: url("../../../public/edit-icon.svg");
+                  background-size: contain;
+                  background-repeat: no-repeat;
+                  background-position: center;
+              }
 
               .user-profile-picture {
                   width: 8rem;
