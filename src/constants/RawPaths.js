7c4a27ffcf880126674af5befb2c0c0d64ded942
TrueVale <srvleh@gmail.com>
2023-05-15 13:03:03 +0200

    [DEV-0044] Cart basic setup

--- src/constants/RawPaths.js
+++ src/constants/RawPaths.js
@@ -6,7 +6,9 @@ const RawPaths = {
     EDIT_BOOK: '/edit-book/',
     LOG_IN: '/login',
     SIGN_UP: '/signup',
-    PROFILE_PAGE: '/profile_page'
+    PROFILE_PAGE: '/profile_page',
+    EDIT_PROFILE_PIC: '/edit_profile_pic',
+    CART: '/cart'
 }
 
 export default RawPaths
