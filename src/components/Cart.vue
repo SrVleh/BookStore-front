bee1c7c2e786da90c1b662c5dffce6b77b5ab023
TrueVale <srvleh@gmail.com>
2023-05-25 13:14:43 +0200

    [DEV-0054] Show items on cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="page-container">
       <div class="order-info-container" v-if="store.state.isOngoingOrder">
+          <div class="books-container" >
+              <div class="book" v-for="book in books" :key="book.id" :style="{ backgroundImage: `url(${ book.image_url })`}">
+                  <div class="book-info">
+                      <h2 class="title">{{ book.title }}</h2>
+                      <p class="author">{{ book.author }}</p>
+                      <div class="actions">
+                          <p class="price">{{ book.price }}€</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
           <router-link :to="Paths.PROFILE_PAGE" class="btn action-btn"
                        @click="completePurchase">Complete purchase</router-link>
       </div>
@@ -15,11 +27,41 @@
   import OrdersController from "../controllers/OrdersController.js";
   import Paths from "../constants/Paths.js";
   import {store} from "../state/index.js";
-  import {onMounted} from "vue";
+  import {onMounted, ref} from "vue";
+  import TokenController from "../controllers/TokenController.js";
+  import BooksController from "../controllers/BooksController.js";
 
-  onMounted(() => {
+  const books = ref([])
+  const ordered_books = ref({})
 
+  onMounted(async() => {
+      ordered_books.value = await test()
+      OrdersController.CheckOngoingOrder().then( orders => {
+        ordered_books.value = ordered_books.value.filter(ob => ob.order_id === orders[0].id)
+        console.log(ordered_books.value)
+        ordered_books.value.forEach(order => {
+            console.log(order.book_id)
+            storeBooksToArray(order.book_id)
+            //books.value.push()
+        })
+    })
   })
+
+  const storeBooksToArray = async (id) => {
+      let book = await BooksController.GetBook(id)
+      books.value.push(book)
+  }
+
+  const test = async() => {
+      const res = await fetch("http://localhost:3000/ordered_books", {
+          method: 'GET',
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: TokenController.GetToken()
+          }
+      })
+      return res.json()
+  }
 
   const completePurchase = () => {
       OrdersController.CheckOngoingOrder().then((order) => {
@@ -37,6 +79,30 @@
       align-items: center;
       width: 100%;
       height: 100%;
+      gap: 1rem;
+
+      .books-container {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-evenly;
+          width: 100%;
+          height: auto;
+
+          .book {
+              display: flex;
+              flex-direction: column;
+              align-content: flex-end;
+              justify-content: flex-end;
+              width: 18rem;
+              height: 14rem;
+              border: 1px solid white;
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;
+              border-radius: 8px;
+
+          }
+      }
   }
 
   .no-order-container{
