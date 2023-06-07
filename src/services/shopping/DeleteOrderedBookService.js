89eafe3f82586ea6e2e23527c958d500d282e799
TrueVale <srvleh@gmail.com>
2023-06-07 12:59:50 +0200

    [DEV-0064] Delete and Update OrderedBooks

--- src/services/shopping/DeleteOrderedBookService.js
+++ src/services/shopping/DeleteOrderedBookService.js
@@ -2,24 +2,21 @@ import TokenController from "../../controllers/TokenController.js";
 
 const API_URL = "http://localhost:3000/ordered_books/"
 
-class UpdateOrderedBookService {
-    static Call(ordered_book, quantity) {
-        return this.UpdateBook(ordered_book, quantity)
+class DeleteOrderedBookService {
+    static Call(ordered_book) {
+        return this.UpdateBook(ordered_book)
     }
 
-    static UpdateBook = async(ordered_book, quantity) => {
+    static UpdateBook = async(ordered_book) => {
         const res = await fetch(API_URL + ordered_book.id, {
-            method: 'PUT',
+            method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: TokenController.GetToken()
-            },
-            body: JSON.stringify({
-                quantity: quantity
-            })
+            }
         })
         return res
     }
 }
 
-export default UpdateOrderedBookService
+export default DeleteOrderedBookService
