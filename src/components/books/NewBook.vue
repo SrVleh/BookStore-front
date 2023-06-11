042300b2ab1cd5eab61aab8455fb9c0da526a37f
TrueVale <srvleh@gmail.com>
2023-06-11 18:15:43 +0200

    [DEV-0071] Add category to books

--- src/components/books/NewBook.vue
+++ src/components/books/NewBook.vue
@@ -4,6 +4,9 @@
             <h1>New book</h1>
             <input v-model="title" class="custom-input" type="text" placeholder="Title">
             <input v-model="author" class="custom-input" type="text" placeholder="Author">
+            <select class="category-selector" v-model="selected_category">
+                <option v-for="category in BOOKS_CATEGORIES">{{ category }}</option>
+            </select>
             <input v-model="price" class="custom-input" type="number" placeholder="0">
             <input v-model="image_url" class="custom-input" type="url" placeholder="Image url">
             <textarea rows="15" v-model="synopsis" class="custom-area" type="text" placeholder="Synopsis" />
@@ -19,12 +22,14 @@
 import {ref} from 'vue';
 import RouterController from "../../controllers/RouterController.js";
 import Paths from "../../constants/Paths.js";
+import BOOKS_CATEGORIES from "../../services/books/category/BooksCategoriesService.js";
 
 const title = ref("")
 const author = ref("")
 const price = ref(0)
 const image_url = ref("")
 const synopsis = ref("")
+const selected_category = ref(BOOKS_CATEGORIES[0])
 const API_URL = "http://localhost:3000/books"
 
 const createBook = async() => {
@@ -36,6 +41,7 @@ const createBook = async() => {
         body: JSON.stringify({
             title: title.value,
             author: author.value,
+            category: selected_category.value,
             price: price.value,
             image_url: image_url.value,
             synopsis: synopsis.value
